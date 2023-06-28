bafad7ca02a0344ff4f570e20bff6d2f6d793e5c
James Best <jim@justjibba.net>
2023-06-28 08:15:08 +0100

    Add extension to UIViewController to show alert

---
+++ GithubFollowers/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GithubFollowers
+//
+//  Created by James Best on 28/06/2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    // You are not allowed to show UI elements from the background thread
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(alertTitle: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
