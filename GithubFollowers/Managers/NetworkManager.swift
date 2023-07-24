e886d4e4310ffe5f835ef13bab3b02c6af4d548f
James Best <jim@justjibba.net>
2023-07-24 20:45:13 +0100

    Add user network call code

--- GithubFollowers/Managers/NetworkManager.swift
+++ GithubFollowers/Managers/NetworkManager.swift
@@ -18,6 +18,8 @@ class NetworkManager {
     private init() {}
     
     // @escaping is our callback/completionHandler
+    // @escaping makes the closure outlive the calling function has finished
+    // closure is non escaping by default
     // GFError is our enum
     func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseUrl + "\(username)/followers?per_page=20&page=\(page)"
@@ -55,6 +57,45 @@ class NetworkManager {
         }
         
         
+       
+        // this starts the call
+        task.resume()
+    }
+    
+    func getUserInfo(for username: String, completed: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseUrl + "\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                // This error is usually because you dont have internet
+                // http errors come back in the response
+                completed(.failure(.networkError))
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
        
         // this starts the call
         task.resume()
--- GithubFollowers/Model/User.swift
+++ GithubFollowers/Model/User.swift
@@ -42,15 +42,15 @@
 import Foundation
 
 struct User: Codable {
-    var login: String
-    var avatarUrl: String
+    let login: String
+    let avatarUrl: String
     var name: String?
     var location: String?
     var bio: String?
-    var publicRepos: Int
-    var publicGists: Int
-    var followers: Int
-    var following: Int
-    var htmlUrl: String
-    var createdAt: String
+    let publicRepos: Int
+    let publicGists: Int
+    let followers: Int
+    let following: Int
+    let htmlUrl: String
+    let createdAt: String
 }
